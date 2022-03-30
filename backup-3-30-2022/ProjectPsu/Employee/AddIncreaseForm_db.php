a95c627fc1b40d016da98571969cb1f1b832bced
armza332 <armzach673@gmail.com>
2022-03-30 15:53:43 +0700

    UpdateBackup

---
+++ backup-3-30-2022/ProjectPsu/Employee/AddIncreaseForm_db.php
@@ -0,0 +1,66 @@
+<?php 
+
+    session_start();
+    require_once 'config/db.php';
+
+    if (isset($_POST['principal'])) {
+       $id = $_POST['id'];
+       $idcard = $_POST['idcard'];
+       $pf_type = $_POST['pf_type'];
+       $old_interestpaid = $_POST['old_interestpaid'];
+       $inc_interestpaid = $_POST['inc_interestpaid'];
+       $current_interestpaid = $_POST['current_interestpaid'];
+       $newprice = $_POST['newinc'];
+       $pf_price = $_POST['pf_price'];
+       $oldpf_creatat = $_POST['oldpf_creatat'];
+       $inc_expdate = $_POST['inc_expdate'];
+       $comment = $_POST['comment'];
+    $pf_interestpaid = $old_interestpaid+$inc_interestpaid;
+    $current_interest = $current_interestpaid-$ppay_interestpaid;
+$newinc = $pf_price+$newprice;
+       if (empty($newinc)) {
+        $_SESSION['error'] = 'กรุณากรอกจำนวนดอกเบี้ย';
+        header("location: AddIncreaseForm.php?continueID=$id");
+    }
+    
+    
+
+    else {
+        try {
+
+           if (!isset($_SESSION['error'])) {
+        $stmt = $conn->prepare("INSERT INTO incprincipal(id, idcard, pf_type, inc_interestpaid, inc_expdate, comment, newinc, oldpf_creatat) VALUES (:id, :idcard, :pf_type, :inc_interestpaid, :inc_expdate, :comment , :newinc, :oldpf_creatat)");
+        $stmt -> bindParam(":id",$id);
+        $stmt -> bindParam(":idcard",$idcard);
+        $stmt -> bindParam(":pf_type",$pf_type);
+        $stmt -> bindParam(":inc_interestpaid",$inc_interestpaid);
+        $stmt -> bindParam(":inc_expdate",$inc_expdate);
+        $stmt -> bindParam(":oldpf_creatat",$oldpf_creatat);
+        $stmt -> bindParam(":comment",$comment);
+        $stmt -> bindParam(":newinc",$newprice);
+        $stmt -> execute();
+        $pf = $conn->query("UPDATE pawnform SET pf_interestpaid = $pf_interestpaid WHERE id = $id");
+        $price = $conn->query("UPDATE pawnform SET pf_price = $newinc WHERE id = $id");
+        $price -> execute();
+        $interest = $conn->query("UPDATE pawnform SET current_interestpaid = $current_interest WHERE id = $id");
+        $interest -> execute();
+        $pf2 = $conn->prepare("UPDATE pawnform SET pf_expdate = :pf_expdate WHERE id = $id");
+        $pf2 -> bindParam(":pf_expdate",$inc_expdate);
+        $pf->execute();
+        $pf2->execute();
+        
+        $_SESSION['success'] = "ทำการเพิ่มเงินต้นเรียบร้อยแล้ว";
+                   header("location: AddIncreaseForm.php?continueID=$id ");
+                } else {
+                    $_SESSION['error'] = "มีบางอย่างผิดพลาด";
+                    header("location: AddIncreaseForm.php?continueID=$id ");
+                   }
+    
+                } catch(PDOException $e) {
+                    echo $e->getMessage();
+                }
+            }
+        }
+
+
+        
---
+++ backup-3-30-2022/ProjectPsu/Employee/Addcontinue_db.php
@@ -0,0 +1,57 @@
+<?php 
+
+    session_start();
+    require_once 'config/db.php';
+
+    if (isset($_POST['continue'])) {
+       $id = $_POST['id'];
+       $idcard = $_POST['idcard'];
+       $pf_type = $_POST['pf_type'];
+       $pf_price = $_POST['pf_price'];
+       $old_interestpaid = $_POST['old_interestpaid'];
+       $con_interestpaid = $_POST['con_interestpaid'];
+       $pf_expdate = $_POST['newpf_expdate'];
+       $old_expdate = $_POST['pf_expdate'];
+       $comment = $_POST['comment'];
+    $pf_interestpaid = $old_interestpaid+$con_interestpaid;
+    $current_interest = $current_interestpaid+$con_interestpaid;
+       if (empty($con_interestpaid)) {
+        $_SESSION['error'] = 'กรุณากรอกจำนวนดอกเบี้ย';
+        header("location: Addcontinue.php?continueID=$id");
+    } 
+    
+    
+
+    else {
+        try {
+
+           if (!isset($_SESSION['error'])) {
+        $stmt = $conn->prepare("INSERT INTO continueint(id, idcard, pf_price, pf_type, con_interestpaid, new_expdate, comment, old_expdate) VALUES (:id, :idcard, :pf_price, :pf_type, :con_interestpaid, :new_expdate, :comment ,:old_expdate)");
+        $stmt -> bindParam(":id",$id);
+        $stmt -> bindParam(":idcard",$idcard);
+        $stmt -> bindParam(":pf_price",$pf_price);
+        $stmt -> bindParam(":pf_type",$pf_type);
+        $stmt -> bindParam(":con_interestpaid",$con_interestpaid);
+        $stmt -> bindParam(":new_expdate",$pf_expdate);
+        $stmt -> bindParam(":old_expdate",$old_expdate);
+        $stmt -> bindParam(":comment",$comment);
+        $stmt -> execute();
+        $pf = $conn->query("UPDATE pawnform SET pf_interestpaid = $pf_interestpaid WHERE id = $id");
+        $pf2 = $conn->prepare("UPDATE pawnform SET pf_expdate = :pf_expdate  WHERE id = $id");
+        $pf2 -> bindParam(":pf_expdate",$pf_expdate);
+        $pf->execute();
+        $pf2->execute();
+        $interest = $conn->query("UPDATE pawnform SET current_interestpaid = $current_interest WHERE id = $id");
+        $interest -> execute();
+        $_SESSION['success'] = "ทำการต่อดอกเบี้ยเรียบร้อยแล้ว";
+                   header("location: Addcontinue.php?continueID=$id ");
+                } else {
+                    $_SESSION['error'] = "มีบางอย่างผิดพลาด";
+                    header("location: Addcontinue.php?continueID=$id ");
+                   }
+    
+                } catch(PDOException $e) {
+                    echo $e->getMessage();
+                }
+            }
+        }
